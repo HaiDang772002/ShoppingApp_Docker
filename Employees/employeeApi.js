0750dc30a939bbe08eb56edae9dd6d2cf05d102b
HaiDang <toisedaudaihoc27@gmail.com>
2023-08-21 20:44:48 +0700

    Third

--- Employees/employeeApi.js
+++ Employees/employeeApi.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const employeeModel = require('./employeeModel');
 const Employee = require('./employeeModel');
-const Shipper = require('../Shippers/shipperModel');
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
